1e1a1265a5a8315f4b8c7e4edb73e2a35ec07808
SoraKumo <info@croud.jp>
2019-12-15 13:21:29 +0900

    文字を赤に設定 (+1 squashed commits)
    Squashed commits:
    
    [a204aaf] 背景色を白に変更
    Squashed commits:
    
    [e3b9377] Storyを増やす
    
    Squashed commits:
    
    [01c0cf6] 背景色を白に変更

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -17,13 +17,35 @@ storiesOf("Welcome", module)
     );
   })
   .add("to Storybook2", () => {
-    const label:string = Knobs.text("ラベル", "ふぉっふぉっふぉ");
+    const label: string = Knobs.text("ラベル", "ふぉっふぉっふぉ");
     console.log("start");
     return (
       <div>
         テスト
-        <Test01 label={label}
-        onClick={action("onClick")} />
+        <Test01 label={label} onClick={action("onClick")} />
+      </div>
+    );
+  });
+
+  storiesOf("Story2", module)
+  .addDecorator(Knobs.withKnobs)
+  .add("to Storybook", () => {
+    const label = Knobs.text("ラベル", "あいうえお");
+    console.log("start");
+    return (
+      <div>
+        テスト
+        <Test01 label={label} onClick={action("onClick")} />
+      </div>
+    );
+  })
+  .add("to Storybook2", () => {
+    const label: string = Knobs.text("ラベル", "ふぉっふぉっふぉ");
+    console.log("start");
+    return (
+      <div>
+        テスト
+        <Test01 label={label} onClick={action("onClick")} />
       </div>
     );
   });
